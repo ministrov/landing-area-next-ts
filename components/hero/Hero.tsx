213ebab1d5c3a21cc60d5c441825612459ef5c27
Zhila <ministrov2018@gmail.com>
2025-10-31 15:11:35 +0300

    refactore: refactore a realization with a dynamic use of images on diff viewports

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,12 +1,34 @@
+'use client';
+
 import Image from 'next/image';
+import { useState, useEffect } from 'react';
 import styles from './Hero.module.css';
 
 export const Hero = () => {
+  const [windowWidth, setWindowWidth] = useState<number>(0);
+
+  console.log(setWindowWidth);
+
+  useEffect(() => { }, []);
+
+  let src = '/hero-desktop.png';
+  let width = 907;
+  let height = 644;
+
+  if (windowWidth <= 800) {
+    src = '/hero-mobile.png';
+    width = 345;
+    height = 434;
+  } else if (windowWidth <= 1280) {
+    src = '/hero-tablet.png';
+    width = 768;
+    height = 544;
+  }
+
   return (
     <section className={styles.hero}>
       <h2 className={styles.title}>Browse everything.</h2>
-
-      <Image className={styles.image} src={'/hero-desktop.png'} width={907} height={644} alt={''} />
+      <Image className={styles.image} src={src} width={width} height={height} alt={''} />
       <div className={styles.background}></div>
     </section>
   )
