7253f9284ac90881de0edae52935034e706cea87
Zhila <ministrov2018@gmail.com>
2025-10-31 16:20:08 +0300

    feat: export a partners logo from figma maket

--- components/partners/Partners.tsx
+++ components/partners/Partners.tsx
@@ -2,6 +2,13 @@ import styles from './Partners.module.css';
 
 export const Partners = () => {
   return (
-    <div className={styles.partners}>Partners</div>
+    <section className={styles.partners}>
+      <h2 className='visually-hidden'>Partner logo images section</h2>
+      <p className={styles.slogan}>Trusted by:</p>
+
+      <ul className={styles.partnersList}>
+
+      </ul>
+    </section>
   )
 }
