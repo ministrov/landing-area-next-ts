2206b33ee814195d8e3096be30612840ca888e6a
Zhila <ministrov2018@gmail.com>
2025-11-03 15:57:26 +0300

    feat: start creating a new section benefits, add a component structure to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from '@/components/hero/Hero';
 import { Partners } from '@/components/partners/Partners';
+import { Benefits } from '@/components/benefits/Benefits';
 
 export default function Home() {
   return (
     <div className='container'>
       <Hero />
       <Partners />
+      <Benefits />
     </div>
   );
 }
---
+++ components/benefits/Benefits.tsx
@@ -0,0 +1,7 @@
+import styles from './Benefits.module.css';
+
+export const Benefits = () => {
+  return (
+    <div className={styles.benefits}>Benefits</div>
+  )
+}
