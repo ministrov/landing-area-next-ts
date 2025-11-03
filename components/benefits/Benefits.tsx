27d5e120ecb91a9d6a3798a36b936055ab836335
Zhila <ministrov2018@gmail.com>
2025-11-03 16:06:03 +0300

    feat: export images from figma for benefits section

--- components/benefits/Benefits.tsx
+++ components/benefits/Benefits.tsx
@@ -1,7 +1,20 @@
+import Image from 'next/image';
 import styles from './Benefits.module.css';
 
 export const Benefits = () => {
   return (
-    <div className={styles.benefits}>Benefits</div>
+    <section className={styles.benefits}>
+      <header>
+        <h2 className={styles.heading}>Benefits</h2>
+        <p>We’ve cracked the code.</p>
+        <p>Area provides real insights, without the data overload.</p>
+      </header>
+
+      <ul>
+        <li></li>
+      </ul>
+
+      <Image src="/benefits-image-desktop.jpg" width={1200} height={620} alt="" />
+    </section>
   )
 }
