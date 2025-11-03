cafc6523fed1103ba76bec762a38f3dbe350547d
Zhila <ministrov2018@gmail.com>
2025-11-03 17:16:19 +0300

    feat: create a markup for list in benefits section

--- components/benefits/Benefits.tsx
+++ components/benefits/Benefits.tsx
@@ -10,8 +10,27 @@ export const Benefits = () => {
         <p className={styles.insights}>Area provides real insights, without the data overload.</p>
       </header>
 
-      <ul>
-        <li></li>
+      <ul className={styles.list}>
+        <li className={styles.item}>
+          <Image src={'/benefits-insights.svg'} width={24} height={24} alt={''} />
+          <h3>Amplify Insights</h3>
+          <p>Unlock data-driven decisions with comprehensive analytics, revealing key opportunities for strategic regional growth.</p>
+        </li>
+        <li className={styles.item}>
+          <Image src={'/benefits-insights.svg'} width={24} height={24} alt={''} />
+          <h3>Control Your Global Presence</h3>
+          <p>Manage and track satellite offices, ensuring consistent performance and streamlined operations everywhere.</p>
+        </li>
+        <li className={styles.item}>
+          <Image src={'/benefits-insights.svg'} width={24} height={24} alt={''} />
+          <h3>Remove Language Barriers</h3>
+          <p>Adapt to diverse markets with built-in localization for clear communication and enhanced user experience.</p>
+        </li>
+        <li className={styles.item}>
+          <Image src={'/benefits-insights.svg'} width={24} height={24} alt={''} />
+          <h3>Visualize Growth</h3>
+          <p>Generate precise, visually compelling reports that illustrate your growth trajectories across all regions.</p>
+        </li>
       </ul>
 
       <Image src="/benefits-image-desktop.jpg" width={1200} height={620} alt="" />
