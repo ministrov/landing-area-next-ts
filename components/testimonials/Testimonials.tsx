26ff590609b0caebccb5f1beef5473eb0fbd1daf
Zhila <ministrov2018@gmail.com>
2025-11-04 13:03:57 +0300

    feat: create a markup structure for testimonials section and add a testimonials array of carousel data

--- components/testimonials/Testimonials.tsx
+++ components/testimonials/Testimonials.tsx
@@ -1,7 +1,22 @@
+import Image from 'next/image';
+import { testimonials } from '@/helpers';
 import styles from './Testimonials.module.css';
 
 export const Testimonials = () => {
+  const { image, author, text, role } = testimonials[0];
+
   return (
-    <div className={styles.testimonials}>Testimonials</div>
+    <section className={styles.testimonials}>
+      <div className={styles.carousel}>
+        <div className={styles.slide}>
+          <Image src={image} width={590} height={670} alt={author} className={styles.avatar} />
+          <div className={styles.content}>
+            <p className={styles.text}>{text}</p>
+            <p className={styles.author}>{author}</p>
+            <p className={styles.role}>{role}</p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
