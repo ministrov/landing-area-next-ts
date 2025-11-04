45b8328c16cf60fc37a0dd0cedd8e34f5874aad8
Zhila <ministrov2018@gmail.com>
2025-11-04 10:08:08 +0300

    feat: create a markup for features section

--- components/features/Features.tsx
+++ components/features/Features.tsx
@@ -1,7 +1,27 @@
+import Image from 'next/image';
+import { Button } from '../burtton/Button';
 import styles from './Features.module.css';
 
 export const Features = () => {
   return (
-    <div className={styles.features}>Features</div>
+    <section className={styles.features}>
+      <div className={styles.text}>
+        <h2 className={styles.title}>See the Big Picture</h2>
+
+        <p className={styles.slogan}>Area turns your data into clear, vibrant visuals that show you exactly what&apos;s happening in each region.</p>
+
+        <ul className={styles.list}>
+          <li className={styles.item}></li>
+          <li className={styles.item}></li>
+          <li className={styles.item}></li>
+          <li className={styles.item}></li>
+        </ul>
+
+        <Button>Discover More</Button>
+      </div>
+      <div className={styles.wrapperImage}>
+        <Image src={''} alt={''} />
+      </div>
+    </section>
   )
 }
