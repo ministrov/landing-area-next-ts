9ba69a7ea249ea9e2c1e4223a742802ba9ab42da
Zhila <ministrov2018@gmail.com>
2025-11-04 11:28:17 +0300

    feat: create a markup for spec section

--- components/specification/Specification.tsx
+++ components/specification/Specification.tsx
@@ -1,7 +1,21 @@
+import { Button } from '../burtton/Button';
 import styles from './Specification.module.css';
 
 export const Specification = () => {
   return (
-    <div className={styles.spec}>Specification</div>
+    <section className={styles.spec}>
+      <div className={styles.heading}>
+        <h2 className={styles.title}>Specs</h2>
+        <p>Why Choose Area?</p>
+        <p>You need a solution that keeps up. That’s why we developed Area. A developer-friendly approach to streamline your business. </p>
+        <Button>Discover More</Button>
+      </div>
+
+      <ul className={styles.list}>
+        <li className={styles.item}></li>
+        <li className={styles.item}></li>
+        <li className={styles.item}></li>
+      </ul>
+    </section>
   )
 }
