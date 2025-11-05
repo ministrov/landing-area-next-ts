5df8fa2f2ddeb20b3ab0b10b142db994d315ab6a
Zhila <ministrov2018@gmail.com>
2025-11-05 16:47:24 +0300

    feat: start operating on adjustments in section spec to create a list and sublist

--- components/specification/Specification.tsx
+++ components/specification/Specification.tsx
@@ -12,9 +12,40 @@ export const Specification = () => {
       </div>
 
       <ul className={styles.list}>
-        <li className={styles.item}></li>
-        <li className={styles.item}></li>
-        <li className={styles.item}></li>
+        <li className={styles.item}>
+          Area
+          <ul>
+            <li>Ultra-fast browsing</li>
+            <li>Advanced AI insights</li>
+            <li>Seamless integration</li>
+            <li>Advanced AI insights</li>
+            <li>Ultra-fast browsing</li>
+            <li>Full UTF-8 support</li>
+          </ul>
+        </li>
+        <li className={styles.item}>
+          WebSurge
+          <ul>
+            <li>Fast browsing</li>
+            <li>Basic AI recommendations</li>
+            <li>Restricts customization</li>
+            <li>Basic AI insights</li>
+            <li>Fast browsing</li>
+            <li>Potential display errors</li>
+          </ul>
+        </li>
+        <li className={styles.item}>
+          HyperView
+
+          <ul>
+            <li>Moderate speeds</li>
+            <li>No AI assistance</li>
+            <li>Steep learning curve</li>
+            <li>No AI assistance</li>
+            <li>Moderate speeds</li>
+            <li>Partial UTF-8 support</li>
+          </ul>
+        </li>
       </ul>
     </section>
   )
