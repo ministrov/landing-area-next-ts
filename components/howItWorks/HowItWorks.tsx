27778aa545c090130c6e7ead6a206b822e3a0360
Zhila <ministrov2018@gmail.com>
2025-11-05 11:04:51 +0300

    feat: create a markup structure for how it works section

--- components/howItWorks/HowItWorks.tsx
+++ components/howItWorks/HowItWorks.tsx
@@ -1,7 +1,35 @@
+import Image from 'next/image';
+import { Button } from '../burtton/Button';
 import styles from './HowItWorks.module.css';
 
 export const HowItWorks = () => {
   return (
-    <div className={styles.how}>HowItWorks</div>
+    <section className={styles.how}>
+      <header className={styles.header}>
+        <h2 className={styles.title}>Map Your Success</h2>
+
+        <Button>Discover More</Button>
+      </header>
+
+      <ul className={styles.list}>
+        <li className={styles.item}>
+          <span>01</span>
+          <h3>Get Started</h3>
+          <p>With our intuitive setup, you’re up and running in minutes.</p>
+        </li>
+        <li className={styles.item}>
+          <span>02</span>
+          <h3>Customize and Configure</h3>
+          <p>Adapt Area to your specific requirements and preferences.</p>
+        </li>
+        <li className={styles.item}>
+          <span>03</span>
+          <h3>Grow Your Business</h3>
+          <p>Make informed decisions to exceed your goals.</p>
+        </li>
+      </ul>
+
+      <Image src={''} alt={''} />
+    </section>
   )
 }
