fcdbc0af1bf88e2ae5862ef513bdc63910336e8c
Zhila <ministrov2018@gmail.com>
2025-11-05 13:59:43 +0300

    feat: create a markup and styling for connect us section

--- components/connectUs/ConnectUs.tsx
+++ components/connectUs/ConnectUs.tsx
@@ -1,7 +1,14 @@
+import { Button } from '../burtton/Button';
 import styles from './ConnectUs.module.css';
 
 export const ConnectUs = () => {
   return (
-    <div className={styles.connect}>ConnectUs</div>
+    <section className={styles.connect}>
+      <div className={styles.wrapper}>
+        <h2>Connect with us</h2>
+        <p>Schedule a quick call to learn how Area can turn your regional data into a powerful advantage.</p>
+        <Button varient='medium'>Learn More</Button>
+      </div>
+    </section>
   )
 }
