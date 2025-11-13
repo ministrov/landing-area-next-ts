d5747d655d20c6b0185880e71241df991a6e9243
Zhila <ministrov2018@gmail.com>
2025-11-13 16:30:44 +0300

    feat: create a basic functionality for up component

--- components/up/Up.tsx
+++ components/up/Up.tsx
@@ -1,10 +1,19 @@
+'use client';
+
 import Image from 'next/image';
 import up from '../../public/up.svg';
 import styles from './Up.module.css';
 
 export const Up = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
   return (
-    <button className={styles.up}>
+    <button className={styles.up} onClick={scrollToTop}>
       <Image src={up} width={15} height={15} alt={''} />
     </button>
   )
