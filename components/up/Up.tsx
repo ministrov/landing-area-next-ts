7288fbf1b1918ca86e75ea15653b00005604845e
Zhila <ministrov2018@gmail.com>
2025-11-13 14:21:01 +0300

    feat: add up component to layout to see how it render

--- components/up/Up.tsx
+++ components/up/Up.tsx
@@ -1,7 +1,10 @@
+import Image from 'next/image';
 import styles from './Up.module.css';
 
 export const Up = () => {
   return (
-    <div className={styles.up}>Up</div>
+    <div className={styles.up}>
+      <Image src={'/up.svg'} width={10} height={10} alt={''} />
+    </div>
   )
 }
