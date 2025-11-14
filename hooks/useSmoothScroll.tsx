a5455564c088d2e260f0452a8cdcd4d885044afd
Zhila <ministrov2018@gmail.com>
2025-11-14 13:49:44 +0300

    feat: create a hook for a new logic of scrolling to section

---
+++ hooks/useSmoothScroll.tsx
@@ -0,0 +1,19 @@
+import { useCallback } from 'react';
+
+export const useSmoothScroll = () => {
+  const scrollToSection = useCallback((sectionId: string) => {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      const headerHeight = 80; // Высота вашего хедера
+      const elementPosition = element.getBoundingClientRect().top;
+      const offsetPosition = elementPosition + window.pageYOffset - headerHeight;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+      });
+    }
+  }, []);
+
+  return { scrollToSection }
+}
