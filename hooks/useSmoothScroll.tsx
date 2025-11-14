85d56c83660c69e1cce000619031d0784b7b9174
Zhila <ministrov2018@gmail.com>
2025-11-14 16:45:48 +0300

    fix: delete the console log statement

--- hooks/useSmoothScroll.tsx
+++ hooks/useSmoothScroll.tsx
@@ -3,12 +3,11 @@ import { useCallback } from 'react';
 export const useSmoothScroll = () => {
   const scrollToSection = useCallback((sectionId: string) => {
     const element = document.getElementById(sectionId);
-    console.log(element);
+
     if (element) {
       const headerHeight = 148; // Высота вашего хедера
       const elementPosition = element.getBoundingClientRect().top;
       const offsetPosition = elementPosition + window.pageYOffset - headerHeight;
-      console.log(offsetPosition);
 
       window.scrollTo({
         top: offsetPosition,
